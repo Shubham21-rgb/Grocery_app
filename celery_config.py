88d2c25f6301ca22d32be188548045f07ede8aaf
Shubham21-rgb <shubhamrick65@gamil.com>
2025-06-29 09:27:48 +0000

    done

--- celery_config.py
+++ celery_config.py
@@ -1,4 +1,11 @@
-broker_url="redis://localhost:6379/0"
-result_backend="redis://localhost:6379/1"
-Timezone="Asia/Kolkata"
-broker_connection_retry_on_startup=True
+import os
+
+broker_url = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+result_backend = os.environ.get("RESULT_BACKEND", broker_url)
+
+timezone = os.environ.get("CELERY_TIMEZONE", "Asia/Kolkata")
+broker_connection_retry_on_startup = True
+
+accept_content = ['json']
+task_serializer = 'json'
+result_serializer = 'json'
